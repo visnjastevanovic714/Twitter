d4d11a60e5bf082abb456f76286ecf8beb5baca0
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-04-05 18:37:57 +0200

    Napisana JavaDoc dokumentacija za klasu TwitterPoruka, njene atribute i metode.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,24 +1,69 @@
 package com.twitter.poruke;
 
+/**
+ * 
+ * @author Visnja Stevanovic
+ *
+ */
+
 public class TwitterPoruka {
 
+	/**
+	 * Ime korisnika
+	 */
 	private String korisnik;
+
+	/**
+	 * Tekst poruke
+	 */
 	private String poruka;
 
+	/**
+	 * Metoda vraca ime korisnika
+	 * 
+	 * @return ime korisnika
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 
+	/**
+	 * Metoda postavlja ime korisnika
+	 * 
+	 * @param korisnik
+	 * @throws java.lang.RuntimeException
+	 *             ako je uneta vrednost atributa korisnik
+	 *             <ul>
+	 *             <li>null
+	 *             <li>prazan String
+	 *             </ul>
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik == "")
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Metoda vraca tekst poruke
+	 * 
+	 * @return tekst poruke
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 
+	/**
+	 * Metoda postavlja tekst poruke
+	 * 
+	 * @param poruka
+	 * * @throws java.lang.RuntimeException
+	 *             ako je uneta vrednost atributa poruka
+	 *             <ul>
+	 *             <li>null
+	 *             <li>duzina veca od 140 karaktera
+	 *             </ul>
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka == null || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
